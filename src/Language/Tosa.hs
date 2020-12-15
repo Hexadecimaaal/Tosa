7d3cfd8a9a1a7055e4d1a4f8a9166ecbe84b2fd6
Hexadecimal Hyuga <hexadecimaaal@gmail.com>
2020-12-15 15:50:16 +0800

    start writing haskell things

--- src/Language/Tosa.hs
+++ src/Language/Tosa.hs
@@ -1,43 +1,65 @@
-module Language.Tosa (
-
+module Language.Tosa 
+( Expression (..)
 ) where
 
-import Data.Map (Map, empty, member, insert, (!))
+import Data.Map (Map, empty, member, insert, (!), findWithDefault)
+import Data.List (intercalate)
+import qualified Data.Text as T
 
-data Expression =
-    Name [Char]
-    | Lambda [Char] Expression
+data Expression 
+    = Name T.Text
+    | Lambda T.Text Expression
     | Quote Expression
-    deriving (Show, Eq, Ord)
+    | Program [Expression]
+    deriving (Eq, Ord, Show)
+
+-- instance Show Expression where
+--     show (Name s) = T.unpack s
+--     show (Lambda b o) = "\\" ++ T.unpack b ++ ". " ++ show o
+--     show (Quote o) = "(" ++ show o ++ ")"
+--     show (Program l) = "<<" ++ intercalate " " (map show l) ++ ">>"
 
 type Stack = [Expression]
 
-type Context = Map [Char] Expression
+instance {-# OVERLAPPING #-} Show Stack where
+    show [] = "(Empty Stack)"
+    show s = concatMap (++"\n") $ reverse $ zipWith (++) (map ((++": ") . show) [1..]) $ map show s
 
-data Error =
-    IsValue
+type Bindings = Map T.Text Expression
+
+type Context = (Bindings, Stack)
+
+data Error 
+    = IsValue
     | NotApplicatable
     | StackEmptied
     deriving (Show, Eq)
 
--- under given Context and Stack, evaluate Expression, i.e.
+-- under given Bindings and Stack, evaluate Expression, i.e.
 -- if it's a function then apply it, else push it into the stack
-eval :: Context -> Stack -> Expression -> Either Error Stack
+eval :: Context -> Expression -> Either Error Context
 
 -- evaluating "EVAL" will pop the stack, evaluate.
-eval c [] (Name "EVAL") = Left StackEmptied
-eval c (h:t) (Name "EVAL") = eval c t h
+eval (b, [])    (Name "EVAL") = Left StackEmptied
+eval (b, (h:t)) (Name "EVAL") = eval (b, t) h
 
--- evaluating a generic name will match it in Context and
+-- evaluating a generic name will match it in Bindings and
 -- substitute if it's defined.
-eval c s (Name n) = if member n c then Right $ (c ! n) : s else Right $ (Name n) : s
+eval (b, s) (Name n) = Right $ (b,) $ (:s) $ findWithDefault (Name n) n b
 
 -- evaluating a Lambda will pop the stack, and
 -- apply to the given Lambda.
-eval c [] (Lambda _ _) = Left StackEmptied
-eval c (h:t) (Lambda b o) = eval (insert b h c) t o
+eval (b, [])    (Lambda _ _) = Left StackEmptied
+eval (b, (h:t)) (Lambda f o) = eval ((insert f h b), t) o
 
 -- evaluating a Quote will push the Quoted contents
 -- onto the stack, *without* evaluating them.
 -- evaluate "EVAL" to force.
-eval c s (Quote o) = Right $ o : s
+eval (b, s) (Quote o) = Right $ (b, o : s)
+
+-- evaluating a series Program: evaluate the first step,
+-- use the new environment to evaluate the rest.
+eval (b, s) (Program [])    = Right (b, s)
+eval (b, s) (Program (h:t)) = do
+    c <- eval (b, s) h
+    eval c $ Program t
--- src/Language/Tosa/Parser.hs
+++ src/Language/Tosa/Parser.hs
@@ -2,4 +2,48 @@ module Language.Tosa.Parser (
 
 ) where
 
+import Language.Tosa (Expression(..))
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Char (isAsciiLower, isAsciiUpper, isDigit)
+import Data.Void
+import qualified Text.Megaparsec.Char.Lexer as L
+import qualified Data.Text as T
+
+type Parser = Parsec Void T.Text
+
+sc :: Parser ()
+sc = L.space
+  space1
+  (L.skipLineComment "#")
+  (L.skipBlockCommentNested "(#" "#)")
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: T.Text -> Parser T.Text
+symbol = L.symbol sc
+
+atom :: String -> Parser a -> Parser a
+atom lbl = label lbl . lexeme
+
+nameWord :: Parser T.Text
+nameWord = T.cons <$> satisfy isStart <*> takeWhileP Nothing isEnd
+    where
+        isStart c = isAsciiUpper c || isAsciiLower c || any (==c) ("_!$%&-=^~|\\/*?<>" :: [Char])
+        isEnd c = isStart c || isDigit c
+
+lexName :: Parser Expression
+lexName = Name <$> atom "name" nameWord
+
+parseQuote :: Parser Expression
+parseQuote = Quote <$> between (char '(') (char ')') parseExpression
+
+parseProgram :: Parser Expression
+parseProgram = Program <$> between (string "<<") (string ">>") (many parseExpression)
+
+parseExpression :: Parser Expression
+parseExpression = try parseProgram <|> try parseQuote <|> lexName
+
+
 
